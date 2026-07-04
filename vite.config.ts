6125ae852c7281919a152c06777abb6c610d1fc4
Noxus-afk <jose.perez@dn50.mx>
2026-07-04 13:09:32 -0600

    chore: update nitro preset to vercel and disable ssr in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,9 @@ export default defineConfig({
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
     server: { entry: "server" },
+    ssr: false,
   },
   nitro: {
-    preset: "vercel-edge"
+    preset: "vercel",
   },
 });
